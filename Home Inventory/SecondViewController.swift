6a4e08c2d757303e3c6f52bc7793b9a8077de823
Jason Shultz <jasshultz@gmail.com>
2015-10-02 16:14:57 -0600

    Initial Commit

---
+++ Home Inventory/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Home Inventory
+//
+//  Created by Jason Shultz on 10/2/15.
+//  Copyright © 2015 HashRocket. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
