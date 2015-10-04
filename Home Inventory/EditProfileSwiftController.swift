a0d748ddb07f25f7a056620ec8a5154fb6d26e42
Jason Shultz <jasshultz@gmail.com>
2015-10-04 14:43:22 -0600

    more work.

--- Home Inventory/EditProfileSwiftController.swift
+++ Home Inventory/EditProfileSwiftController.swift
@@ -7,13 +7,10 @@
 //
 
 import UIKit
+import CoreData
 
 class EditProfileSwiftController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
     
     @IBOutlet weak var firstNameField: UITextField!
     
@@ -32,8 +29,91 @@ class EditProfileSwiftController: UIViewController {
     
     @IBOutlet weak var zipField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        let request = NSFetchRequest(entityName: "Profile")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.executeFetchRequest(request)
+            
+            if (results.count > 0) {
+                
+                for result in results as! [NSManagedObject] {
+                    if let fname:String = result.valueForKey("fName") as? String { // cast username as String so we can use it.
+                        firstNameField.text = String(UTF8String: fname)!
+                    } else {
+                        firstNameField.text = ""
+                    }
+                    if let lname = result.valueForKey("lName") as? String { // cast username as String so we can use it.
+                        lastNameField.text = String(lname)
+                    } else {
+                        lastNameField.text = ""
+                    }
+                    if let address = result.valueForKey("address") as? String { // cast username as String so we can use it.
+                        streetAddressField.text = String(address)
+                    } else {
+                        streetAddressField.text = ""
+                    }
+                    if let city = result.valueForKey("city") as? String { // cast username as String so we can use it.
+                        cityField.text = String(city)
+                    } else {
+                        cityField.text = ""
+                    }
+                    if let state = result.valueForKey("state") as? String { // cast username as String so we can use it.
+                        stateField.text = String(state)
+                    } else {
+                        stateField.text = ""
+                    }
+                    if let zip = result.valueForKey("zip") as? String { // cast username as String so we can use it.
+                        zipField.text = String(zip)
+                    } else {
+                        zipField.text = ""
+                    }
+                }
+                
+            }
+        } catch {
+            print("something went wrong")
+        }
+        
+    }
     
     @IBAction func profileButton(sender: AnyObject) {
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        let fName:String = String(firstNameField.text!)
+        let lName:String = String(lastNameField.text!)
+        let address:String = String(streetAddressField.text!)
+        let city:String = String(cityField.text!)
+        let state:String = String(stateField.text!)
+        let zip:String = String(zipField.text!)
+        
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Profile", inManagedObjectContext: context)
+        
+        newUser.setValue(fName, forKey: "fName")
+        newUser.setValue(lName, forKey: "lName")
+        newUser.setValue(address, forKey: "address")
+        newUser.setValue(city, forKey: "city")
+        newUser.setValue(state, forKey: "state")
+        newUser.setValue(zip, forKey: "zip")
+        
+        do {
+            try context.save()
+        } catch {
+            print("Something went wrong")
+        }
+        
+        
     }
     
     
--- Home Inventory/SecondViewController.swift
+++ Home Inventory/SecondViewController.swift
@@ -7,12 +7,81 @@
 //
 
 import UIKit
+import CoreData
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var fNameLabel: UILabel!
+    
+    @IBOutlet weak var lNameLabel: UILabel!
+    
+    @IBOutlet weak var addressLabel: UILabel!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var stateLabel: UILabel!
+    
+    @IBOutlet weak var zipLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDel: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+        let context: NSManagedObjectContext = appDel.managedObjectContext
+        
+        let request = NSFetchRequest(entityName: "Profile")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            let results = try context.executeFetchRequest(request)
+            
+            if (results.count > 0) {
+                
+                for result in results as! [NSManagedObject] {
+                    if let fname:String = result.valueForKey("fName") as? String { // cast username as String so we can use it.
+                        fNameLabel.text = String(UTF8String: fname)!
+                    } else {
+                        fNameLabel.text = ""
+                    }
+                    if let lname = result.valueForKey("lName") as? String { // cast username as String so we can use it.
+                        lNameLabel.text = String(lname)
+                    } else {
+                        lNameLabel.text = ""
+                    }
+                    if let address = result.valueForKey("address") as? String { // cast username as String so we can use it.
+                        addressLabel.text = String(address)
+                    } else {
+                        addressLabel.text = ""
+                    }
+                    if let city = result.valueForKey("city") as? String { // cast username as String so we can use it.
+                        cityLabel.text = String(city)
+                    } else {
+                        cityLabel.text = ""
+                    }
+                    if let state = result.valueForKey("state") as? String { // cast username as String so we can use it.
+                        stateLabel.text = String(state)
+                    } else {
+                        stateLabel.text = ""
+                    }
+                    if let zip = result.valueForKey("zip") as? String { // cast username as String so we can use it.
+                        print(zip)
+//                        zipLabel.text = "bling"
+                        zipLabel.text = zip
+                    } else {
+                        zipLabel.text = ""
+                    }
+                }
+                
+            }
+        } catch {
+            print("something went wrong")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
